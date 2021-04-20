3105aa49edac6b4cb970f40c4ac0e0e299605e9b
warrioravi <abhisinghraj2k18@gmail.com>
2021-04-21 01:12:44 +0530

    completed ccommand 1 and 2

--- wcat.js
+++ wcat.js
@@ -4,9 +4,7 @@ console.log("input : ",input);
 let options=[];
 let filepath=[];
 for(let i=0;i<input.length;i++){
-    //first character in string
-   let firstchar=input[i].charAt(0);
-   if(firstchar=='-'){
+   if( input[i]=="-s" || input[i]=="-b" || input[i] == "-n" ){
       options.push(input[i]);
    }
    else{
@@ -15,3 +13,16 @@ for(let i=0;i<input.length;i++){
 }
 console.log(options);
 console.log(filepath);
+for(let i=0;i<filepath.length;i++){
+   let isFilePresent=fs.existsSync(filepath[i]);
+   if(!isFilePresent){
+      console.log("filepath", filepaths[i]),"does not exist";
+      return;
+   }
+}
+let file="";
+for(let i=0;i<filepath.length;i++){
+  file=file+fs.readFileSync(filepath[i]).toString();
+}
+console.log(file);
+

dd7f6a2a0868bcbb337f7fad6c08af041598828d
warrioravi <abhisinghraj2k18@gmail.com>
2021-04-21 06:39:40 +0530

    implemented command -b

--- wcat.js
+++ wcat.js
@@ -1,9 +1,10 @@
 let fs= require('fs')
-let input=process.argv.slice(2);
+let input=process.argv.slice(2);//taking input from the user
 console.log("input : ",input);
-let options=[];
-let filepath=[];
+let options=[];//array for storing options of the command
+let filepath=[];// array for storing paths of the files
 for(let i=0;i<input.length;i++){
+   //identifying input arguments if they are option or filepath
    if( input[i]=="-s" || input[i]=="-b" || input[i] == "-n" ){
       options.push(input[i]);
    }
@@ -16,21 +17,22 @@ console.log(filepath);
 for(let i=0;i<filepath.length;i++){
    let isFilePresent=fs.existsSync(filepath[i]);
    if(!isFilePresent){
-      console.log("filepath", filepaths[i]),"does not exist";
+      console.log("filepath: ", filepaths[i]," does not exist.Kindly check path");
       return;
    }
 }
+// to read content from provided files and concatenating them
 let totalcontent="";
 for(let i=0;i<filepath.length;i++){
-  totalcontent=totalcontent+fs.readFileSync(filepath[i]).toString()+"\n";
+  totalcontent=totalcontent+fs.readFileSync(filepath[i]).toString()+"\r\n";
 }
-// Implementing command 3(-s) 
+// Implementing command(-s) >> Removes Empty line breaks
 let isSOption=options.includes('-s');
 if(isSOption){
-   //empty line break remove
-   //console.log totalcontent
-   //identify empty line breaks
+   
+   //identify and remove empty line breaks
    let contentarray=totalcontent.split("\r\n");
+
    console.log(contentarray);
    //remove  
    let tempArr=[];
@@ -39,10 +41,10 @@ if(isSOption){
             tempArr.push(contentarray[i]);
         }
    }
-   totalcontent=tempArr.join("\n");
+   totalcontent=tempArr.join("\r\n");
 
 }
-//console.log(finalfile);
+// Implementing command(-n) >> Puts a number to every line
 let isN=options.includes("-n");
 if(isN){
    let count=1;
@@ -51,6 +53,22 @@ if(isN){
    for(let i=0;i<contentArr.length;i++){
       contentArr[i]=count+" ."+contentArr[i];
       count++;
+   }
+
+   totalcontent = contentArr.join("\r\n");
+}
+
+let isB=options.includes("-b");
+if(isB){
+   let count=1;
+   let contentArr=totalcontent.split("\r\n");
+
+   for(let i=0;i<contentArr.length;i++){
+      if(contentArr[i]!=""){
+         contentArr[i]=count+" ."+contentArr[i];
+         count++;
+      }
+      
    }
 
    totalcontent = contentArr.join("\r\n");
